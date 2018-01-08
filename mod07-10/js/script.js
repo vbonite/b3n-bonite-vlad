95c5ec1c8737a8aacc6c8a514b31f409996ada43
Vlademir Bonite <vlad.bonite@gmail.com>
2018-01-08 21:07:48 +0800

    mod07-10

--- mod07-10/js/script.js
+++ mod07-10/js/script.js
@@ -21,6 +21,6 @@ $("input[type='text']").keypress(function(event) {
 	if (event.which === 13) {  //13 == enter key
 		var todoText = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span class='glyphicon glyphicon-trash'></span> " + todoText + "</li>");
 	}
 });
