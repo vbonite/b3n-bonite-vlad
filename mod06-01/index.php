42303ead591e4872774d10b02a11033c301d8319
Vlademir Bonite <vlad.bonite@gmail.com>
2018-01-10 18:23:01 +0800

    PHP Programming mod06-01 and mod06-02

---
+++ mod06-01/index.php
@@ -0,0 +1,70 @@
+<?php  
+
+	// echo "Welcome to PHP Programming";
+
+	// phpinfo();
+
+	// $name = "Vlad Bonite";
+
+	// print "Good Morning $name";
+
+	// echo "Today is ", date("F"), "." ;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+
+	<title>Welcome to PHP Programming</title>
+</head>
+<body>
+
+	<?php 
+		// $msg = 'Welcome to PHP Programming';
+		// echo "$msg";
+
+		// $username = 'Vlad Bonite';
+		// echo $username;
+		// echo '<br>';
+		// $username = 'John Doe';
+		// $current_user = $username;
+		// echo $current_user;
+
+
+		// echo '<br>';
+		// echo 'Buzz Lightyear once said: "You\'re a toy!" <br>';
+
+		// echo 'You deleted C:\\*.*';
+
+		// $number1 = 150;
+		// $number2 = 33.5;
+		// $sum = $number1 + $number2;
+		// $product = $number1 * $number2;
+		// $difference = $number1 - $number2;
+		// $quotient = $number1 / $number2;
+
+		// echo "Sum is: $sum <br>";
+		// echo "Product is: $product <br>";
+		// echo "Difference is: $difference <br>";
+		// echo "Quotient: $quotient <br>";
+
+		$members = array('Kato', 'Shem', 'Angeli', 'Ali', 'Carmela');
+		$colors = array('red', 'green', 'blue');
+		$fruits = array('mango', 'apple', 'avocado');
+
+		echo $members[4];
+
+		echo $colors[0];
+
+		echo $fruits[1];
+
+		echo $fruits[2];
+	 ?>
+
+</body>
+</html>
---
+++ mod06-02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+
+	<title>PHP Programming, Printing and Variables</title>
+
+</head>
+<body>
+
+	<?php 
+
+		$lyrics = array('stars', 'shine', 'yellow');
+
+		echo "Look at the $lyrics[0], look how they $lyrics[1] for you, and everything you do.<br>";
+		echo "Yeah, they were all $lyrics[2].";
+
+	 ?>
+
+</body>
+</html>
