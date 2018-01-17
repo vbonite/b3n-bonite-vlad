f261201f6f7c67d6a3d39ea53a3dbfabcb668b02
Vlademir Bonite <vlad.bonite@gmail.com>
2018-01-17 21:32:53 +0800

    csp template

---
+++ csp2/home.php
@@ -0,0 +1,23 @@
+<?php 
+
+	function getTitle() {
+		echo "Index | Welcome to Beeer Web App";
+	}
+
+	include "partials/head.php";
+	
+?>
+
+</head>
+<body>
+
+	<main class="wrapper">
+		<h1>Home</h1>
+	</main>
+
+
+<?php 
+
+	include "partials/foot.php"
+
+?>
---
+++ csp2/partials/foot.php
@@ -0,0 +1,10 @@
+
+	<!-- import jquery -->
+	<script type="text/javascript" src="assets/lib/jquery-3.2.1.min.js"></script>
+
+	<script type="text/javascript" src="assets/js/script.js"></script>
+
+	<script type="text/javascript" src="assets/css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
